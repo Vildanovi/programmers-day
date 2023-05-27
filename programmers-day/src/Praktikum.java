7cbeb550e0f819f0c97a36d18c8159bb12ddabda
Vildanovi <vildanovi@yandex.ru>
2023-05-27 21:21:21 +0300

    Добавил проврку кратности 4

--- programmers-day/src/Praktikum.java
+++ programmers-day/src/Praktikum.java
@@ -17,7 +17,7 @@ public class Praktikum {
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        if (year % 400 == 0) {
+        if (year % 400 == 0 || year % 4 == 0) {
             return true;
         } else {
             return false;
